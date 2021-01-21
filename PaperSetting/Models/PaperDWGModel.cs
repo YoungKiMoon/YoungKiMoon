e7aa7eec7ab654325ac49305ee8cd00c8619e243
eden kim <surinaver@naver.com>
2021-01-21 14:35:03 +0900

    2021-01-21

---
+++ PaperSetting/Models/PaperDWGModel.cs
@@ -0,0 +1,23 @@
+﻿using PaperSetting.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PaperSetting.Models
+{
+    public class PaperDWGModel : Notifier, ICloneable
+    {
+        public PaperDWGModel()
+        {
+
+        }
+
+
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
