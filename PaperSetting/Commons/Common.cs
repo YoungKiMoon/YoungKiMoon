834f48aa13719a8607806373b22587ed7a6dd411
eden kim <surinaver@naver.com>
2021-01-27 09:02:13 +0900

    2021-01-27

---
+++ PaperSetting/Commons/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperSetting.Commons
+{
+    public enum PAPERFORMAT_TYPE
+    {
+        A0_ISO, A1_ISO, A2_ISO, A3_ISO, A4_ISO, A4_LANDSCAPE_ISO, A_ANSI, A_LANDSCAPE_ANSI, B_ANSI, C_ANSI, D_ANSI, E_ANSI
+    }
+}
